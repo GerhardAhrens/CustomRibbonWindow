025a1d8a23dac6e6902f4b6a79bb5036f917cb9f
Gerhard Ahrens <gerhard.ahrens@pta.de>
2024-05-27 08:19:25 +0200

    *

--- CustomRibbonWindow/App.xaml.cs
+++ CustomRibbonWindow/App.xaml.cs
@@ -1,17 +1,144 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace CustomizedTitle
+﻿namespace CustomizedTitle
 {
+    #region using's 😊 
+    using System;
+    using System.Collections;
+    using System.Diagnostics;
+    using System.Globalization;
+    using System.IO;
+    using System.Text;
+    using System.Threading;
+    using System.Windows;
+    using System.Windows.Markup;
+    using System.Windows.Threading;
+    #endregion using's
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        private const string DEFAULTLANGUAGE = "de-DE";
+        private const string SHORTNAME = "APM.Tool";
+        private static readonly string MessageBoxTitle = "APM.Tool Application";
+        private static readonly string UnexpectedError = "An unexpected error occured.";
+        private string exePath = string.Empty;
+        private string exeName = string.Empty;
+
+        public App()
+        {
+            /* Name der EXE Datei*/
+            exeName = Path.GetFileNameWithoutExtension(AppDomain.CurrentDomain.FriendlyName);
+            /* Pfad der EXE-Datei*/
+            exePath = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory);
+            /* Synchronisieren einer Textenigabe mit dem primären Windows (wegen Validierung von Eingaben)*/
+            FrameworkCompatibilityPreferences.KeepTextBoxDisplaySynchronizedWithTextProperty = false;
+
+            try
+            {
+                /* Initalisierung Spracheinstellung */
+                InitializeCultures(DEFAULTLANGUAGE);
+            }
+            catch (Exception ex)
+            {
+                ex.Data.Add("UserDomainName", Environment.UserDomainName);
+                ex.Data.Add("UserName", Environment.UserName);
+                ex.Data.Add("exePath", exePath);
+                ErrorMessage(ex, "General Error: ");
+                ApplicationExit();
+            }
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
+
+        private void InitializeCultures(string language)
+        {
+            if (string.IsNullOrEmpty(language) == false)
+            {
+                Thread.CurrentThread.CurrentCulture = new CultureInfo(language);
+            }
+            else
+            {
+                Thread.CurrentThread.CurrentCulture = new CultureInfo(DEFAULTLANGUAGE);
+            }
+
+            if (string.IsNullOrEmpty(language) == false)
+            {
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(language);
+            }
+            else
+            {
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(DEFAULTLANGUAGE);
+            }
+
+            FrameworkPropertyMetadata frameworkMetadata = new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(new CultureInfo(language).IetfLanguageTag));
+            FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), frameworkMetadata);
+        }
+
+
+        public static void ErrorMessage(Exception ex, string message = "")
+        {
+            string expMsg = ex.Message;
+            var aex = ex as AggregateException;
+
+            if (aex != null && aex.InnerExceptions.Count == 1)
+            {
+                expMsg = aex.InnerExceptions[0].Message;
+            }
+
+            if (string.IsNullOrEmpty(message) == true)
+            {
+                message = UnexpectedError;
+            }
+
+            StringBuilder errorText = new StringBuilder();
+            if (ex.Data != null && ex.Data.Count > 0)
+            {
+                foreach (DictionaryEntry item in ex.Data)
+                {
+                    errorText.AppendLine($"{item.Key} : {item.Value}");
+                }
+            }
+
+            MessageBox.Show(
+                message + $"{expMsg}\n{ex.Message}\n{errorText.ToString()}",
+                MessageBoxTitle,
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+        }
+
+        public static void InfoMessage(string message)
+        {
+            MessageBox.Show(
+                message,
+                MessageBoxTitle,
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+        }
+
+        /// <summary>
+        /// Screen zum aktualisieren zwingen, Globale Funktion
+        /// </summary>
+        public static void DoEvents()
+        {
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new Action(delegate { }));
+        }
+
+        private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            Debug.WriteLine($"{exeName}-{(e.Exception as Exception).Message}");
+        }
+
+        /// <summary>
+        /// Programmende erzwingen
+        /// </summary>
+        public static void ApplicationExit()
+        {
+            Application.Current.Shutdown();
+            Process.GetCurrentProcess().Kill();
+        }
     }
 }
